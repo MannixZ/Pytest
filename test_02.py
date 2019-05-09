f9b3a2b6642d43663ecfceff14053b4b7439e53a
曾明杰 <wb.zengmingjie@mesg.corp.netease.com>
2019-05-09 20:35:43 +0800

    update pytest

--- test_02.py
+++ test_02.py
@@ -20,4 +20,12 @@ def login(request):
     print('登录账户：%s' %user)
     return user
 
-@pytest.mark.parametrize('login', test_user_data)
+@pytest.mark.parametrize('login', test_user_data, indirect=True)
+def test_login(login):
+    '''登录用例'''
+    a = login
+    print('测试用例中login的返回值为 %s' %a)
+    assert a != ""
+
+if __name__ == '__main__':
+    pytest.main(['-s', 'test_02.py'])
